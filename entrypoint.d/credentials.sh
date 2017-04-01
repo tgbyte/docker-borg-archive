b98b2ca00fcab8cb21f4eabc1f6070f13e787691
Thilo-Alexander Ginkel <tg@tgbyte.de>
2017-04-01 23:03:24 +0200

    Change schedule, don't delete

--- entrypoint.d/credentials.sh
+++ entrypoint.d/credentials.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "open -u ${FTP_USER},${FTP_PASSWORD} ${FTP_HOST}
 mkdir -p ${FTP_DIR}
-mirror -c -e -R /var/backups/borg ${FTP_DIR}
+mirror -c -R --loop /var/backups/borg ${FTP_DIR}
 exit" > /backup/backup.lftp
 
 mkdir -p /root/.ssh
