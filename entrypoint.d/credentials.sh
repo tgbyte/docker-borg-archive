93a10b9d2c6aeef7a94fc2165a5428e29c1f1d24
Thilo-Alexander Ginkel <tg@tgbyte.de>
2017-10-17 14:37:21 +0200

    Add BORG_UID/BORG_GID support

--- entrypoint.d/credentials.sh
+++ entrypoint.d/credentials.sh
@@ -4,5 +4,14 @@ mkdir -p ${FTP_DIR}
 mirror -c -R --loop /var/backups/borg ${FTP_DIR}
 exit" > /backup/backup.lftp
 
+if [ -n "${BORG_UID}" ]; then
+    usermod -u ${BORG_UID} borg
+fi
+
+if [ -n "${BORG_GID}" ]; then
+    groupmod -o -g ${BORG_GID} borg
+    usermod -g ${BORG_GID} borg
+fi
+
 mkdir -p /root/.ssh
 echo "${KNOWN_HOSTS}" > /root/.ssh/known_hosts
